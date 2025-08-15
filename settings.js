f9234774dfd08c48586c47fbf629838eaf368a5b
icool9611-beep <icool9611@gmail.com>
2025-08-15 23:32:16 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Coolice Bot",
   botOwner: 'Coolice', // Your name
   ownerNumber: '233536714265', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
